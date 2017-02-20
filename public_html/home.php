cb65b675548a236d2a2612a6a56200e4ecb035e4
Arnaldo <arnaldo.govene@outlook.com>
2017-02-21 01:43:58 +0200

    fixing mysql query procedure

--- public_html/home.php
+++ public_html/home.php
@@ -8,11 +8,11 @@ if (!$session->is_logged_in()){redirect_to('index.php');}
 /**
   * Add dates to calendar
 **/
-$cur_date = date("Ymd");
-$update_date = date("Y-12-20");
-if($cur_date >= $update_date){
-  $database->add_to_calendar();
-}
+// $cur_date = date("Ymd");
+// $update_date = date("Y-12-20");
+// if($cur_date >= $update_date){
+//   $database->add_to_calendar();
+// }
 
 ?>
 
